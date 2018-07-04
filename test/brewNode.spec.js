08f9941dc0533d9e02ef1b4cc1992c58677f1260
William <webdevelopper@protonmail.ch>
2018-07-04 12:56:22 +0800

    update

--- test/brewNode.spec.js
+++ test/brewNode.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const async = require('async');
+const cluster = require('cluster');
+
 const BrewNode = require('../brewNode');
 
 class Node {
@@ -17,34 +18,3 @@ class Node {
   }
 }
 
-async.parallel(
-  {
-    one: function() {
-      new Node(8081, 8087);
-    },
-    two: function() {
-      new Node(8082, 8081);
-    },
-    three: function() {
-      new Node(8083, 8082);
-    },
-    four: function() {
-      new Node(8084, 8083);
-    },
-    five: function() {
-      new Node(8085, 8084);
-    },
-    six: function() {
-      new Node(8086, 8085);
-    },
-    seven: function() {
-      new Node(8087, 8086);
-    }
-  },
-  (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(`result:\n${result}`);
-  }
-);
